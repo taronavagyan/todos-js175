f8848283df4d660d9841aa87548ac551e170ed2a
Taron Avagyan <contact@taronavagyan.com>
2022-12-23 09:02:44 -0800

    new list page

--- todos.js
+++ todos.js
@@ -1,11 +1,33 @@
 const express = require("express");
 const morgan = require("morgan");
+const TodoList = require("./lib/todolist");
 
 const app = express();
 const host = "localhost";
 const port = 3000;
 
 let todoLists = require("./lib/seed-data");
+
+const compareByTitle = (todoListA, todoListB) => {
+  let titleA = todoListA.title.toLowerCase();
+  let titleB = todoListB.title.toLowerCase();
+
+  if (titleA < titleB) {
+    return -1;
+  } else if (titleA > titleB) {
+    return 1;
+  } else {
+    return 0;
+  }
+};
+
+const sortTodoLists = (todoLists) => {
+  let undone = todoLists.filter((todoList) => !todoList.isDone());
+  let done = todoLists.filter((todoList) => todoList.isDone());
+  undone.sort(compareByTitle);
+  done.sort(compareByTitle);
+  return [].concat(undone, done);
+};
 
 app.set("views", "./views");
 app.set("view engine", "pug");
@@ -14,7 +36,23 @@ app.use(morgan("common"));
 app.use(express.static("public"));
 
 app.get("/", (req, res) => {
-  res.render("lists", { todoLists });
+  res.redirect("/lists");
+});
+
+app.get("/lists", (req, res) => {
+  res.render("lists", {
+    todoLists: sortTodoLists(todoLists),
+  });
+});
+
+app.get("/lists/new", (req, res) => {
+  res.render("new-list");
+});
+
+app.post("/lists", (req, res) => {
+  let title = req.body.todoListTitle.trim();
+  todoLists.push(new TodoList(title));
+  res.redirect("/lists");
 });
 
 app.listen(port, host, () => {
