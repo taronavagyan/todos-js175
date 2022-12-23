ccd9ea7d3db795aa81b8bacc359c95d08cb046ba
Taron Avagyan <contact@taronavagyan.com>
2022-12-23 01:10:05 -0800

    seed data and code

--- todos.js
+++ todos.js
@@ -5,10 +5,13 @@ const app = express();
 const host = "localhost";
 const port = 3000;
 
+let todoLists = require("./lib/seed-data");
+
 app.set("views", "./views");
 app.set("view engine", "pug");
 
 app.use(morgan("common"));
+app.use(express.static("public"));
 
 app.get("/", (req, res) => {
   res.render("lists");
